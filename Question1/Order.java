6da1c52b744f0b43275449acf4a2a5c2950053e9
harsh <harshhemraz403@gmail.com>
2025-11-03 20:56:50 +0400

    Added Ques1

---
+++ Question1/Order.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab1;
+
+/**
+ *
+ * @author SPACE4
+ */
+public class Order {
+    
+    
+}
