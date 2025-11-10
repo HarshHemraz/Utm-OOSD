03e0d2c826b86be49994c9a0e352b9f90b1815a8
harsh <harshhemraz403@gmail.com>
2025-11-10 10:09:02 +0400

    Added student.java

--- Lab1/Student.java
+++ Lab1/Student.java
@@ -2,41 +2,41 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Lab1;
+package LAB1;
+
 /**
  *
- * @author SPACE4
+ * @author harsh
  */
-
 public class Student {
-    public int student_id;
-    public String student_name;
-    public String student_address;
-    public int contact_number;
+    private int student_id;
+    private String student_name;
+    private String student_address;
+    private int student_number;
     
-public void setStudentID(int id) {
-    student_id = id;
+    public void setStudentId(int id) {
+        student_id = id;
+    }
+    public void setStudentName(String name) {
+        student_name = name;
+    }
+    public void setStudentAddress(String address) {
+        student_address = address;
+    }
+    public void setStudentNumber(int number) {
+        student_number = number;
+    }
+    
+    public int getStudentId(){
+        return student_id;
+    }
+    public String getStudentName(){
+        return student_name;
+    }
+    public String getStudentAddress(){
+        return student_address;
+    }
+    public int getStudentNumber(){
+        return student_number;
+    }
 }
-public void setStudentName(String name) {
-   student_name = name;
-}
-public void setStudentAddress(String address) {
-    student_address = address;
-}
-public void setContantNum(int contactNum) {
-    contact_number = contactNum;
-}
-
-public int getStudentId() {
-    return student_id;
-}
-public String getStudentName() {
-    return student_name;
-}
-public String getStudentAddress() {
-    return student_address;
-}
-public int getContactNumber(){
-    return contact_number;
-}
- }
