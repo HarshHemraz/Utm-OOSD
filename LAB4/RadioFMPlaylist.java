ac7383517012dbc5d79964781cbd24913ccf4a0c
harsh <harshhemraz403@gmail.com>
2025-12-03 14:30:36 +0400

    Added Radio Fm Playlist

--- LAB4/RadioFMPlaylist.java
+++ LAB4/RadioFMPlaylist.java
@@ -378,6 +378,7 @@ public class RadioFMPlaylist extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
